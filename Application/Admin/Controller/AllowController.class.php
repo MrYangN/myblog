dc1fd4a3bf784d7cc0bcd5d6a8a111ac4650ae2c
yangning <ityangning@163.com>
2020-06-10 10:55:46 +0800

    test

--- Application/Admin/Controller/AllowController.class.php
+++ Application/Admin/Controller/AllowController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class AllowController extends Controller {
     public function _initialize(){
     	if(session('is_login1')!='on'){
-    		$this->error("请先登陆后台",U("Login/index"));
+    		$this->error("请先登陆后台11",U("Login/index"));
     	}
     }
 }
